e3da40822e0bf99b0eea882fa1c82bfda0dc2751
qwe <empty>
2023-10-05 00:29:55 +0500

    new fork

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,22 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
+  components:{
+    MeetupAgendaItem
+  },
+  props:{
+    agenda:{
+      require: true,
+      type: Array,
+    }
+  },
 
-  template: `
-    <ul class="agenda">
+  template: `  
+    <ul v-for='item in agenda' class="agenda">
       <li class="agenda__item">
-        <!-- meetup agenda item -->
+        <MeetupAgendaItem :agendaItem="item"/>
       </li>
     </ul>`,
 });
