0be17d4446b1f564a8e48053b673db3143590b37
qsde1 <qsdwy1@gmail.com>
2023-10-29 21:06:39 +0500

    03-sfc

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,22 +1,42 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{title}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+
+  props:{
+    title:{
+      type: String,
+      require: false,
+    },
+    image: {
+      type: String,
+      require: false
+    }
+  },
+
+  computed:{
+    getBackgroundImage(){
+      return this.image ?
+        `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${this.image})`:
+        'var(--default-cover)';
+    }
+  }
 };
 </script>
 
-<style scoped>
+<style scoped setup>
+:root{
+  --default-cover: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("https://course-vue.javascript.ru/api/images/2");
+}
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  background-image: v-bind('getBackgroundImage');
   display: flex;
   flex-direction: column;
   align-items: center;
